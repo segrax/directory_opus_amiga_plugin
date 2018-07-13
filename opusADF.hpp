c2b7d7c91592cdeae0e5ceb98df98a7b3504443f
Robert Crossfield <robcrossfield@gmail.com>
2018-07-13 22:08:10 +1000

    Import files, delete files

--- opusADF.hpp
+++ opusADF.hpp
@@ -1,5 +1,7 @@
+typedef std::shared_ptr<List> spList;
 
 class cADFPluginData {
+
     HINSTANCE				mAdfDll;
     size_t                  mLastError;
     std::string             mPath;
@@ -9,10 +11,12 @@ class cADFPluginData {
 
 protected:
 
-    LPVFSFILEDATAHEADER GetFileData(const Entry *pEntry, HANDLE pHeap);
+    LPVFSFILEDATAHEADER GetVFSforEntry(const Entry *pEntry, HANDLE pHeap);
     FILETIME            GetFileTime(const Entry *pEntry);
+    void                SetEntryTime(File *pFile, FILETIME pFT);
 
-    bool LoadAdf(const std::string& pAfPath);
+    bool LoadFile(const std::string& pAfPath);
+    spList GetCurrentDirectoryList();
 
 public:
     cADFPluginData();
@@ -22,15 +26,25 @@ public:
 
     bool AdfChangeToPath(const std::string& pPath, bool pIgnoreLast = false);
 
-    void FreeDir();
     bool ReadDirectory(LPVFSREADDIRDATAA lpRDD);
     bool ReadFile(File* pFile, size_t pBytes, std::uint8_t* pBuffer );
+
     File* OpenFile(std::string pPath);
     void CloseFile(File* pFile);
  
+    size_t TotalFreeBlocks(const std::string& pFile);
+    size_t TotalDiskBlocks(const std::string& pFile);
+
+    int Delete(LPVFSBATCHDATAA lpBatchData, const std::string& pPath, const std::string& pFile, bool pAll = false);
+
+    int Import(LPVFSBATCHDATAA lpBatchData, const std::string& pFile, const std::string& pPath);
+    int ImportFile(LPVFSBATCHDATAA lpBatchData, const std::string& pFile, const std::string& pPath);
+    int ImportPath(LPVFSBATCHDATAA lpBatchData, const std::string& pFile, const std::string& pPath);
+
     int Extract(const std::string& pFile, const std::string& pDest);
     int ExtractFile(const Entry* pEntry, const std::string& pDest);
     int ExtractPath(const std::string& pPath, const std::string& pDest);
+
     int ContextVerb(LPVFSCONTEXTVERBDATAA lpVerbData);
     UINT BatchOperation(LPTSTR lpszPath, LPVFSBATCHDATAA lpBatchData);
 };
